313df395674056b651b66c15103b53a8be3382a2
Monowar Hasan <mnwr.hsn@gmail.com>
2019-04-20 10:40:42 -0400

    init repo

---
+++ config.mk
@@ -0,0 +1,7 @@
+TEE_SDK_DIR=$(shell pwd)
+TOOLCHAIN_32_PATH := $(TEE_SDK_DIR)/toolchain/aarch32/bin/arm-linux-gnueabihf-
+TOOLCHAIN_64_PATH := $(TEE_SDK_DIR)/toolchain/aarch64/bin/aarch64-linux-gnu-
+
+
+export CROSS_COMPILE := $(TOOLCHAIN_32_PATH)
+export CROSS_COMPILE_AARCH64 := $(TOOLCHAIN_64_PATH)
